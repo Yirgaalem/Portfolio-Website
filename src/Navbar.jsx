e26ba7c58c037075e55509e1b841bf672ede2cf9
Yirgaalem <nahor21@hotmail.com>
2023-02-19 02:47:47 -0500

    a

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar(){
     return (
         <ul className="navbar">
             <li><a className="Home" href="#home">Home</a></li>
-            <li><a className="Projects" href="#projects">Projects</a></li>
+            <li><a className="Projects" href="Second Page">Projects</a></li>
             <li><a className="About" href="#about">About</a></li>
             <li><a className="Resume" href='src\assets\Resume.pdf' target="_blank">Resume</a></li>
             <li><a className="Contact" href="#contact">Contact</a></li>
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,9 +1,26 @@
 import './Projects.css'
+import { Carousel } from '@mantine/carousel';
+
 function Projects(){
     return(
         <div>
             <h1>Projects</h1>
-            <rect width="100" height="100" style="fill:rgb(0,0,255)"></rect>
+            <Carousel
+                withIndicators
+                height={200}
+                slideSize="33.333333%"
+                slideGap="md"
+                breakpoints={[
+                    { maxWidth: 'md', slideSize: '50%' },
+                    { maxWidth: 'sm', slideSize: '100%', slideGap: 0 },
+                ]}
+                loop
+                align="start">
+                <Carousel.Slide>1</Carousel.Slide>
+                <Carousel.Slide>2</Carousel.Slide>
+                <Carousel.Slide>3</Carousel.Slide>
+                
+            </Carousel>
         </div>
         
     )
