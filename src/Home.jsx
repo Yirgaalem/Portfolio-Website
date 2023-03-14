dd7171fc80b54f2b27ac7ee540712c55fe3d7fc5
Nahor Yirgaalem <nahor21@hotmail.com>
2023-03-13 22:56:50 -0400

    added picture, working on getting it to look good

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,12 +3,12 @@ function Home(){
   return(
       
       <div id="Home">
-        
-        <h1>Hi, I'm Nahor Yirgaalem</h1>    
-        <img src="src/assets/desk.png"></img>
-
-        <p>University Student and Full Stack Developer</p>
-        <button className="continue">Continue</button>
+       <div className="container"> 
+          <img className="deskImage"src="src/assets/desk.png"></img>         
+          <h1 className='Hello'>Hi, I'm Nahor Yirgaalem</h1>  
+          <p className='Desc'>University Student and Full Stack Developer</p>
+          <button className="continue">Continue</button>
+        </div>
       </div>
   )
 }
