878295169f544065e82a4d7c87a4765762689caa
Nahor Yirgaalem <nahor21@hotmail.com>
2023-03-20 21:44:47 -0400

    ^

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -7,7 +7,7 @@ function Contact(){
         <div className="github_cont"><a href="https://github.com/Yirgaalem" target="_blank"><img className='github' src='src/assets/socials/github.svg'/></a></div>
         <div className="resume_cont"><a href="src/assets/socials/Resume.pdf" target="_blank"><img className='resume' src='src/assets/socials/resume.png'/></a></div>
       </div>
-      <div className="copy"><img src="src/assets/socials/copyright.png"  className="copyright" /><text className='author'>Nahor Yirgaalem, 2023</text></div>
+      <div className="copy"><p className='author'><img src="src/assets/socials/copyright.png"  className="copyright" />Nahor Yirgaalem, 2023</p></div>
     </div>
   )
 }
